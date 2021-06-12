a895cc6cbce4576c749bfb3c73c1bf9aa2c941ee
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2021-06-12 16:25:02 +0200

    Add sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
-buildscript {
-    repositories {
-        mavenCentral()
+plugins {
+    id("org.sonarqube") version "3.2.0"
+}
+
+allprojects {
+    tasks.withType {
+        val task = this
+        if (task.name == "check") {
+            rootProject.tasks.sonarqube { dependsOn(task) }
+        }
     }
 }
