c61d0ec64a49c0ef25f8d815c96b30952656c3e6
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2021-06-13 10:37:59 +0200

    CI (#7)
    Closes #8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,6 @@
-buildscript {
-    repositories {
-        gradlePluginPortal()
-    }
-
-    dependencies {
-        classpath("dev.lajoscseppento.ruthless:ruthless-plugin:0.2.0")
-    }
+plugins {
+    id("dev.lajoscseppento.ruthless") version "0.2.0"
 }
-
-apply(plugin = "dev.lajoscseppento.ruthless")
 
 rootProject.name = "messy-files-design-data-store"
 include(
