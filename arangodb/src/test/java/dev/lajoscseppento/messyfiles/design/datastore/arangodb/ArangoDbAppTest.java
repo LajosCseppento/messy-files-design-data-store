cf516a15c0bd4310040908511332670f100c29cd
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2021-06-15 23:05:15 +0200

    Disable ArangoDB test

--- arangodb/src/test/java/dev/lajoscseppento/messyfiles/design/datastore/arangodb/ArangoDbAppTest.java
+++ arangodb/src/test/java/dev/lajoscseppento/messyfiles/design/datastore/arangodb/ArangoDbAppTest.java
@@ -1,8 +1,10 @@
 package dev.lajoscseppento.messyfiles.design.datastore.arangodb;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class ArangoDbAppTest {
+  @Disabled
   @Test
   void test() {
     ArangoDbApp.main(new String[0]);
