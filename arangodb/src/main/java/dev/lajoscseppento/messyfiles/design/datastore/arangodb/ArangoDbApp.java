fb8fa94b7b457691999ebe074f280bad1cf92d48
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2021-06-15 23:00:13 +0200

    ArangoDB: Generate documents

--- arangodb/src/main/java/dev/lajoscseppento/messyfiles/design/datastore/arangodb/ArangoDbApp.java
+++ arangodb/src/main/java/dev/lajoscseppento/messyfiles/design/datastore/arangodb/ArangoDbApp.java
@@ -1,9 +1,89 @@
 package dev.lajoscseppento.messyfiles.design.datastore.arangodb;
 
-import dev.lajoscseppento.messyfiles.design.datastore.core.CoreUtils;
+import com.arangodb.ArangoCollection;
+import com.arangodb.ArangoDatabase;
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.entity.CollectionEntity;
+import dev.lajoscseppento.messyfiles.design.datastore.arangodb.database.ArangoDbConnection;
+import dev.lajoscseppento.messyfiles.design.datastore.core.MockFileSystemGenerator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class ArangoDbApp {
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+@SpringBootApplication
+@Slf4j
+public class ArangoDbApp implements CommandLineRunner {
+
+  @Autowired private ArangoDbConnection arangoDbConnection;
+
+  @Autowired private MockFileSystemGenerator mockFileSystemGenerator;
+
+  private ArangoDatabase database;
+
   public static void main(String[] args) {
-    System.out.println("[ArangoDB App] " + CoreUtils.getMessage());
+    SpringApplication.run(ArangoDbApp.class, args).close();
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    database = arangoDbConnection.getDatabase();
+
+    cleanDatabase();
+    generateDatabase();
+    listDatabase();
+  }
+
+  private void listDatabase() {
+    for (CollectionEntity collection : database.getCollections()) {
+      if (Boolean.FALSE.equals(collection.getIsSystem())) {
+        log.info(
+            "Found collection {}, size: {}",
+            collection.getName(),
+            database.collection(collection.getName()).count().getCount());
+      }
+    }
+  }
+
+  private void generateDatabase() throws Exception {
+    log.info("Creating collection...");
+    ArangoCollection collection = database.collection("file-system");
+    collection.create();
+
+    log.info("Populating collection...");
+
+    FileSystem fileSystem = mockFileSystemGenerator.generate();
+
+    for (Path rootDirectory : fileSystem.getRootDirectories()) {
+      Files.walkFileTree(
+          rootDirectory,
+          new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+              BaseDocument document = new BaseDocument();
+
+              document.addAttribute("path", file.toString());
+              document.addAttribute("directory", file.getParent().toString());
+              document.addAttribute("name", file.getFileName().toString());
+
+              collection.insertDocument(document);
+
+              return FileVisitResult.CONTINUE;
+            }
+          });
+    }
+  }
+
+  private void cleanDatabase() {
+    for (CollectionEntity collection : database.getCollections()) {
+      if (Boolean.FALSE.equals(collection.getIsSystem())) {
+        log.info("Deleting collection {}", collection.getName());
+        database.collection(collection.getName()).drop();
+      }
+    }
   }
 }
